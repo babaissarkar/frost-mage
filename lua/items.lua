5520dcba2bd1234e763c7e70368c574f87c79967
Subhraman Sarkar <suvrax@gmail.com>
2024-06-15 22:22:33 +0530

    inventory system : implement show button, add pickup for non-weapon types

--- lua/items.lua
+++ lua/items.lua
@@ -1,4 +1,5 @@
-function format_object (object)
+-- given object, return a nicely formatted string describing it
+function format_object(object)
     local formatted_text
     formatted_text =
         "<span foreground='yellow'><big>"
@@ -7,4 +8,16 @@ function format_object (object)
         ..object['description']
         .."</i>"
     return formatted_text
+end
+
+-- show the stats of a given item object in a gui
+function show_stats_dialog(item_img, details_obj)
+    local preshow = function(dialog)
+        local details = dialog:find('details')
+        details.label = format_object(details_obj[1][2])
+        local image = dialog:find('image')
+        image.label = item_img.."~BLIT(misc/achievement-frames/frame-9-red.png)"
+    end
+
+    return gui.show_dialog(wml.get_child(wml.load("~add-ons/Frost_Mage/gui/item_stats.cfg"), "resolution"), preshow, function() end)
 end
